a66cac7f416d73480c49b499bbcc9f17f6cb1528
kyle wodzicki <krwodzicki@gmail.com>
2019-02-16 10:10:33 -0600

    Allows for missing IMDb id
    This version allows for failure to find IMDb id for TV DVR shows.
    If not IMDb ID is found, it is simply omitted from file name;
    i.e., blank space as in 's00e00 - Ep Name..ext'

--- video_utils/utils/file_rename.py
+++ video_utils/utils/file_rename.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
-
+import logging;
 import os;
 from video_utils.videotagger.metadata.getIMDb_ID import getIMDb_ID;
 
 def file_rename( in_file ):
-  imdbId            = getIMDb_ID( in_file );
-  if imdbId:
-    fileDir           = os.path.dirname(  in_file );
-    fileBase          = os.path.basename( in_file );                              # Get base name of input file
-    series, se, title = fileBase.split(' - ');                                    # Split the file name on ' - '; not header information of function
-    title             = title.split('.');                                         # Split the title on period; this is to get file extension
-    ext               = title[-1];                                                # Get the file extension; last element of list
-    title             = '.'.join(title[:-1]);                                     # Join all but last element of title list using period; will rebuild title
-    new = '{} - {}.{}.{}'.format(se.lower(), title, imdbId, ext);                 # Build new file name
-    new = os.path.join( fileDir, new );                                           # Build new file path
-    os.rename( in_file, new );                                                    # Rename the file
-    return True;
-  return False;
+  log               = logging.getLogger(__name__);
+  log.debug( 'Getting information from file name' );
+
+  fileDir           = os.path.dirname(  in_file );
+  fileBase          = os.path.basename( in_file );                              # Get base name of input file
+  series, se, title = fileBase.split(' - ');                                    # Split the file name on ' - '; not header information of function
+  title             = title.split('.');                                         # Split the title on period; this is to get file extension
+  ext               = title[-1];                                                # Get the file extension; last element of list
+  title             = '.'.join(title[:-1]);                                     # Join all but last element of title list using period; will rebuild title
+  log.debug('Attempting to get IMDb ID')
+  imdbId            = getIMDb_ID( in_file );                                    # Try to get IMDb id
+
+  if not imdbId: 
+    log.warning( 'No IDMb ID! Renaming file without it')
+    imdbId = '';                                                                # If no IMDb id found, set imdbId to emtpy string
+
+  new = '{} - {}.{}.{}'.format(se.lower(), title, imdbId, ext);                 # Build new file name
+  new = os.path.join( fileDir, new );                                           # Build new file path
+  os.rename( in_file, new );                                                    # Rename the file
+  return True;
   
 if __name__ == "__main__":
   import sys;
