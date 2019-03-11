b236e119dc12d5191763394bfe01ce1065685f9e
kyle wodzicki <krwodzicki@gmail.com>
2019-03-11 09:22:39 -0500

    Updates Plex rotating log file setup
    After many issues with popping off keys from the plexFMT dict,
    changed to leaving dictionary intact and using ['key'] where
    needed. Hopefully this solves all problems.

--- video_utils/Plex_DVR_PostProcess.py
+++ video_utils/Plex_DVR_PostProcess.py
@@ -22,36 +22,27 @@ def Plex_DVR_PostProcess(in_file,
      no_remove = False,
      not_srt   = False):
 
-  logName   = plexFMT.pop('name');                                              # Get logger name
   noHandler = True;                                                             # Initialize noHandler to True
   for handler in log.handlers:                                                  # Iterate over all handlers
-    if handler.get_name() == logName:                                           # If handler name matches logName
+    if handler.get_name() == plexFMT['name']:                                   # If handler name matches plexFMT['name']
       noHandler = False;                                                        # Set no handler false
       break;                                                                    # Break for loop
 
-  if noHandler:
-    logFile = plexFMT.pop('file',        None);                                 # Pop off file for logging
-    logFMT  = plexFMT.pop('formatter',   None);                                 # Pop off formatter
-    logLvl  = plexFMT.pop('level',       None);                                 # Pop off the logging level
-    logPerm = plexFMT.pop('permissions', None);                                 # Pop off permissions for the logging file
-    if verbose: logLvl = logging.DEBUG;                                         # If verbose, then set file handler to DEBUG
+  if noHandler:    
+    rfh = RotatingFileHandler(plexFMT['file'], 
+            backupCount = plexFMT['backupCount'],
+            maxBytes    = plexFMT['maxBytes']);                                 # Set up rotating file handler
+    rfh.setFormatter( plexFMT['formatter'] );
+    rfh.setLevel(     plexFMT['level']     );                                   # Set the logging level
+    rfh.set_name(     plexFMT['name']      );                                   # Set the log name
+    log.addHandler( rfh );                                                      # Add hander to the main logger
     
-    if logFile is not None:
-      rfh = RotatingFileHandler(logFile, **plexFMT);                            # Set up rotating file handler
-      if logFMT is not None:
-        rfh.setFormatter( logFMT  );                                            # Set formatter for the handler
-      if logLvl is not None:
-        rfh.setLevel(     logLvl  );                                            # Set the logging level
-      if logName is not None:
-        rfh.set_name(     logName );                                            # Set the log name
-      log.addHandler( rfh );                                                    # Add hander to the main logger
-    
-      info = os.stat( logFile );                                                # Get information about the log file
-      if (info.st_mode & logPerm) != logPerm:                                   # If the permissions of the file are not those requested
-        try:                                                                    # Try to 
-          os.chmod( logFile, logPerm );                                         # Set the permissions of the log file
-        except:
-          log.info('Failed to change log permissions; this may cause issues')
+    info = os.stat( plexFMT['file'] );                                          # Get information about the log file
+    if (info.st_mode & plexFMT['permissions']) != plexFMT['permissions']:       # If the permissions of the file are not those requested
+      try:                                                                      # Try to 
+        os.chmod( plexFMT['file'], plexFMT['permissions'] );                    # Set the permissions of the log file
+      except:
+        log.info('Failed to change log permissions; this may cause issues')
   
   log.info('Input file: {}'.format( in_file ) );
   file = file_rename( in_file );                                                # Try to rename the input file using standard convention
