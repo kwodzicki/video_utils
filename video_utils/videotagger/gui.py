13b83c8696b182e64febf81b18785045f272b47b
Kyle Wodzicki <krwodzicki@gmail.com>
2020-04-28 11:49:00 -0500

    Adds more functionality to GUI, new classes
    The videotagger GUI is currently broken into 3 class, one for the
    search widget, one for the metadata widget, and one for the main
    window. Some metadata boxes have been added, and they update when
    a metadata search is performed.
    
    Improvements:
      - Poster is now resized to 320x160; aspect maintained
    
    To do:
      - Finish added metadata fields
      - Add save button; this will ask for file path if one not defined
      - Add file menu to load a file; metadata fields populated one load.
    
    v1.13.1

--- video_utils/videotagger/gui.py
+++ video_utils/videotagger/gui.py
@@ -1,73 +1,51 @@
 import os
-from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QPushButton, QComboBox, QLineEdit, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QPushButton, QComboBox, QLineEdit, QVBoxLayout, QHBoxLayout, QGridLayout, QAction
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 
+from ..config import APPDIR
 from . import Movie, Episode, utils
 
 HOME                = os.path.expanduser('~')
 SEARCH_ID_TEXT      = 'Series or Movie ID'
 SEARCH_SEASON_TEXT  = 'Season # (optional)'
 SEARCH_EPISODE_TEXT = 'Episode # (optional)' 
+CACHE_DIR           = os.path.join(APPDIR, 'poster_cache')
 
-class VideoTaggerGUI( QMainWindow ):
+os.makedirs(CACHE_DIR, exist_ok = True)
 
+class SearchWidget( QWidget ):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
-    self.__initUI()
-
-  def __initUI(self):
-    self.mainLayout = QVBoxLayout()
-
-    self.__searchBox()
-
-    self.poster     = QLabel()
-    self.mainLayout.addWidget( self.poster )
- 
-    self.mainWidget = QWidget()
-    self.mainWidget.setLayout( self.mainLayout )
-    self.setCentralWidget( self.mainWidget )
-
-  def __searchBox(self):
     layout      = QHBoxLayout()
 
-    options_cb = QComboBox()
-    options_cb.addItem('')
-    options_cb.addItem('TMDb')
-    options_cb.addItem('TVDb')
-    layout.addWidget( options_cb )
+    self.comboBox = QComboBox()
+    self.comboBox.addItem('')
+    self.comboBox.addItem('TMDb')
+    self.comboBox.addItem('TVDb')
+    layout.addWidget( self.comboBox )
 
-    dbID_box   = QLineEdit()
-    dbID_box.setPlaceholderText( SEARCH_ID_TEXT )
-    layout.addWidget( dbID_box )
+    self.dbID_box   = QLineEdit()
+    self.dbID_box.setPlaceholderText( SEARCH_ID_TEXT )
+    layout.addWidget( self.dbID_box )
 
-    season_box = QLineEdit()
-    season_box.setPlaceholderText( SEARCH_SEASON_TEXT ) 
-    layout.addWidget( season_box )
+    self.season_box = QLineEdit()
+    self.season_box.setPlaceholderText( SEARCH_SEASON_TEXT ) 
+    layout.addWidget( self.season_box )
 
-    episode_box = QLineEdit()
-    episode_box.setPlaceholderText( SEARCH_EPISODE_TEXT )
-    layout.addWidget( episode_box )
+    self.episode_box = QLineEdit()
+    self.episode_box.setPlaceholderText( SEARCH_EPISODE_TEXT )
+    layout.addWidget( self.episode_box )
 
-    search_btn = QPushButton('Search')
-    search_btn.clicked.connect( self.getMetadata )
-    layout.addWidget( search_btn )
+    self.search_btn = QPushButton('Search')
+    layout.addWidget( self.search_btn )
 
-    self.search = QWidget()
-    self.search.setLayout( layout )
+    self.setLayout( layout )
 
-    self.mainLayout.addWidget( self.search )
+  def registerFunc(self, func):
+    self.search_btn.clicked.connect( func )
 
-  def getMetadata(self, *args, **kwargs):
-    info = self._dbQuery(self, *args, **kwargs)
-    if info is not None:
-      cover     = info._getCover()
-      if cover:
-        coverFile = utils.downloadCover( cover, saveDir = HOME )
-        if coverFile:    
-          self.poster.setPixmap( QPixmap(coverFile) )
-          os.remove( coverFile )
-
-  def _dbQuery(self, *args, **kwargs):
+  def search(self, *args, **kwargs):
     '''
     Purpose:
       Method to run when the 'Search' button is pushed.
@@ -80,36 +58,25 @@ class VideoTaggerGUI( QMainWindow ):
       An instance of either TVDbMovie, TVDbEpisode, TMDbMovie, TMDbEpisode
       based on search criteria. If bad criteria, then returns None.
     '''
-    comboBox = self.search.findChildren( QComboBox )[0]                                 # Get combobox from search widget
-    dbName   = comboBox.currentText()                                                   # Use current text as db name
+    dbName  = self.comboBox.currentText()                                                   # Use current text as db name
+    dbID    = self.dbID_box.text()
+    season  = self.season_box.text()
+    episode = self.episode_box.text() 
 
-    dbID = season = episode = None                                                      # Default dbID, season, and episode to None
-    lineEdit = self.search.findChildren( QLineEdit )                                    # Get all LineEdit objects in search widget
-    for line in lineEdit:                                                               # Iterate over all LineEdit objects
-      txt   = line.text()                                                               # Get test of the object
-      if txt != '':                                                                     # If text is not empty
-        phTXT = line.placeholderText()                                                  # Get the placeholder text
-        if phTXT == SEARCH_ID_TEXT:                                                     # If placeholder text is the database text
-          dbID = txt                                                                    # Set dbID to current text
-        elif phTXT == SEARCH_SEASON_TEXT:                                               # Else, if the season text
-          season = txt                                                                  # Set season to current text
-        elif phTXT == SEARCH_EPISODE_TEXT:                                              # Else, if episode text
-          episode = txt                                                                 # Set episode to current text
-
-    if dbID is None:                                                                    # If dbID is None
+    if dbID == '':                                                                    # If dbID is None
       print('No database ID set')                                                       # Error
       return None                                                                       # Return None
     else:                                                                               # Else
       args = [dbID]                                                                     # Set args to list with just dbID for now
-      if season is not None and episode is not None:                                    # If both season and episode are NOT None
+      if season != '' and episode != '':                                    # If both season and episode are NOT None
         args.extend( [season, episode] )                                                # Extend the args list
       if dbName == '':                                                                  # If the dbName is empty; i.e., user did not pick a database
         if len(args) == 3:                                                              # If there are 3 arguments
           dbName = 'TVDb'                                                               # Default to TVDb because is tv episode
-          comboBox.setCurrentIndex(2)                                                   # Change comboBox value to be TVDb
+          self.comboBox.setCurrentIndex(2)                                                   # Change comboBox value to be TVDb
         else:                                                                           # Else
           dbName = 'TMDb'                                                               # Set dbName to TMDb
-          comboBox.setCurrentIndex(1)                                                   # Change comboBox value to be TMDb
+          self.comboBox.setCurrentIndex(1)                                                   # Change comboBox value to be TMDb
 
     if dbName == 'TVDb':                                                                # If TVDb
       if len(args) == 3:                                                                # If episode
@@ -125,4 +92,94 @@ class VideoTaggerGUI( QMainWindow ):
       print( 'Error' )
       return None                                                                       # Return None
 
-    return info                                                                         # Return Info
+    return info.metadata()                                                              # Return Info
+
+class MetadataWidget( QWidget ):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.cover = None
+    layout     = QGridLayout()
+
+
+    label      = QLabel('Title')
+    self.title = QLineEdit()
+    self.title.setPlaceholderText( 'Movie or Episode title' )
+    layout.addWidget(label,      0, 0) 
+    layout.addWidget(self.title, 1, 0) 
+
+    label       = QLabel('Series')
+    self.series = QLineEdit()
+    self.series.setPlaceholderText( 'TV series title' )
+    layout.addWidget(label,       2, 0) 
+    layout.addWidget(self.series, 3, 0) 
+
+    label     = QLabel('Year')
+    self.year = QLineEdit()
+    self.year.setPlaceholderText( 'Year of release/airing' )
+    layout.addWidget(label,     4, 0) 
+    layout.addWidget(self.year, 5, 0) 
+
+    label       = QLabel('Poster/Coverart')
+    self.poster = QLabel()
+    self.poster.setFixedSize(320, 180)
+    layout.addWidget( label,       0, 1)
+    layout.addWidget( self.poster, 1, 1, 20, 1)
+
+    self.setLayout( layout )
+
+  def _updateCover(self, info):
+    cover = info.get('cover', None)
+    if cover:
+      coverFile = utils.downloadCover( cover, saveDir = CACHE_DIR )
+      if coverFile:
+        self.cover = coverFile
+        pix = QPixmap(coverFile) 
+        pix = pix.scaled(self.poster.width(), self.poster.height(), Qt.KeepAspectRatio)
+        self.poster.setPixmap( pix )
+    else:
+      self.poster.setPixmap( QPixmap() )
+
+  def updateData(self, info):
+    self.title.setText(  info.get('title',      '') )
+    self.series.setText( info.get('seriesName', '') )
+    self.year.setText(   info.get('year',       '') )
+    #self._updateCover( info )
+
+  def writeData(self, fpath):
+    print(fpath)
+
+class VideoTaggerGUI( QMainWindow ):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.__initUI()
+
+  def __initUI(self):
+    #mainMenu = self.menuBar()
+    #fileMenu = mainMenu.addMenu('File')    
+    #resetButton = QAction()
+    #resetButton.setText('Reset')
+    #resetButton.triggered.connect( self._reset )
+    #mainMenu.addAction( resetButton )
+
+
+    self.mainLayout = QVBoxLayout()
+
+    self.searchWidget = SearchWidget()
+    self.searchWidget.registerFunc( self.getMetadata )
+    self.mainLayout.addWidget( self.searchWidget )
+
+    self.metadataWidget = MetadataWidget()
+    self.mainLayout.addWidget( self.metadataWidget )
+ 
+    self.mainWidget = QWidget()
+    self.mainWidget.setLayout( self.mainLayout )
+    self.setCentralWidget( self.mainWidget )
+
+  def _reset(self, *args, **kwargs):
+    self.metadataWidget.updateData( {} )
+
+  def getMetadata(self, *args, **kwargs):
+    info = self.searchWidget.search(self, *args, **kwargs)
+    if info:
+      self.metadataWidget.updateData( info )
+
