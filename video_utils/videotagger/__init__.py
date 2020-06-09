1694aa5e5223274dc1b2d52b35dc127eb905934f
Kyle Wodzicki <krwodzicki@gmail.com>
2020-06-08 21:36:11 -0500

    Bug fix for Episode parsing in getMetaData
    In the getMetaData method of videotagger.__init__, the regex
    search for seaons/episode pattern vailed to grab the first (only)
    occurance of the pattern from the result. This ment that the result
    was only 1 element instead of 2 (season, episode), causing later
    checks to fail. This made the grabber think the file a movie,
    leading to more breaking.
    
    v2.0.1

--- video_utils/videotagger/__init__.py
+++ video_utils/videotagger/__init__.py
@@ -252,7 +252,7 @@ def getMetaData( file=None, dbID=None, seasonEp=None, version='', **kwargs ):
 
   if file:
     fileDir, fileBase = os.path.split( file )
-    if len(seasonEp) != 2: seasonEp = SEASONEP.findall(fileBase)
+    if len(seasonEp) != 2: seasonEp = SEASONEP.findall(fileBase)[0]
   
     if not isinstance(dbID, str):                                                       # If dbID is NOT a string
       tmp = os.path.splitext(fileBase)[0].split('.')
