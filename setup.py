9ba3e5f859727dd5d881ef92f22127d0512edc90
Kyle Wodzicki <krwodzicki@gmail.com>
2023-09-18 22:24:32 -0400

    Bug fix in setup.py
    Was using the setuptools convert_path, but has been deprecated.
    Moves to using distutils.util.convert_path

--- setup.py
+++ setup.py
@@ -1,58 +1,81 @@
 #!/usr/bin/env python
-import os, shutil, importlib
-from setuptools import setup, find_packages, convert_path
+import os
+import shutil
+import  importlib
+from distutils.util import convert_path
+from setuptools import setup, find_packages
 from setuptools.command.install import install
 
-NAME = "video_utils"
-DESC = "Package for transcoding video files to h264/h265 codec"
+NAME  = "video_utils"
+DESC  = "Package for transcoding video files to h264/h265 codec"
+URL   = "https://github.com/kwodzicki/video_utils"
+AUTH  = "Kyle R. Wodzicki"
+EMAIL = "krwodzicki@gmail.com"
+
+INSTALL_REQUIRES = [
+    "mutagen",
+    "soundfile",
+    "pillow", 
+    "numpy",
+    "scipy",
+    "requests",
+    "psutil",
+    "watchdog",
+    "pyYAML",
+    "pgsrip",
+    "plexapi",
+]
+
+SCRIPTS = [
+    'bin/comremove',
+    'bin/MakeMKV_Watchdog',
+    'bin/Plex_DVR_Watchdog',
+    'bin/rename_media_plex_tag_format',
+    'bin/splitOnChapter',
+    'bin/plexToken',
+    'bin/updateFileNames',
+    'bin/videotagger',
+]
 
 main_ns  = {}
 ver_path = convert_path( "{}/version.py".format(NAME) )
 with open(ver_path) as ver_file:
-  exec(ver_file.read(), main_ns)
+    exec(ver_file.read(), main_ns)
 
 def copyConfig():
-  home = os.path.expanduser('~')
-  pkg  = importlib.import_module(NAME)
-  src  = os.path.join(pkg.DATADIR, 'settings.yml')
-  dst  = os.path.join(home, '.{}.yml'.format(NAME))
-  if os.path.isfile(dst):
-    if os.stat(dst).st_size > 0:
-      return
-  shutil.copy( src, dst)
+
+    home = os.path.expanduser('~')
+    pkg  = importlib.import_module(NAME)
+    src  = os.path.join(pkg.DATADIR, 'settings.yml')
+    dst  = os.path.join(home, '.{}.yml'.format(NAME))
+    if os.path.isfile(dst):
+        if os.stat(dst).st_size > 0:
+            return
+    shutil.copy( src, dst)
 
 class PostInstallCommand(install):
-  """
-  Post-installation for installation mode.
-  Taken from https://stackoverflow.com/questions/20288711/post-install-script-w
-  """
-  def run(self):
-    install.run(self)
-    copyConfig()
+    """
+    Post-installation for installation mode.
+    Taken from https://stackoverflow.com/questions/20288711/post-install-script-w
+    """
+    def run(self):
+
+        install.run(self)
+        copyConfig()
+
 
 setup(
   name                 = NAME,
   description          = DESC,
-  url                  = "https://github.com/kwodzicki/video_utils",
-  author               = "Kyle R. Wodzicki",
-  author_email         = "krwodzicki@gmail.com",
+  url                  = URL,
+  author               = AUTH,
+  author_email         = EMAIL, 
   version              = main_ns['__version__'],
   packages             = find_packages(),
   cmdclass             = {'install' : PostInstallCommand},
   package_data         = {"" : ["*.ini", "*.ttf"]},
   include_package_data = True,
-  install_requires     = [ "mutagen", "soundfile", "pillow", 
-                           "numpy", "scipy",
-                           "requests", "psutil", "watchdog", "pyYAML",
-                           "pgsrip",
-                           "plexapi"],
-  scripts              = ['bin/comremove',
-                          'bin/MakeMKV_Watchdog',
-                          'bin/Plex_DVR_Watchdog',
-                          'bin/rename_media_plex_tag_format',
-                          'bin/splitOnChapter',
-                          'bin/plexToken',
-                          'bin/updateFileNames',
-                          'bin/videotagger'],
-  zip_safe             = False
+  install_requires     = INSTALL_REQUIRES,
+  scripts              = SCRIPTS,
+  zip_safe             = False,
 )
